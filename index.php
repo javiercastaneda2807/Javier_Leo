27c71ed0ca5981e68a6aa9c535d95b713183abce
Javier Castañeda <javier.castaneda@tvirtual.net>
2023-05-26 12:10:13 -0400

    agarra ahí

---
+++ index.php
@@ -0,0 +1,200 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>APEP</title>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <!-- Iconos cdn-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <div class="img" ><img  src="/Javier_Leo/apep.png"></div>
+            <h1>A.P.E.P "La Candelaria"</h1>
+        </header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="col-sm-6">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/Javier_Leo/index.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                </ol>
+            </div>
+            <div class="col-sm-6">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="buscar" placeholder="Buscar" aria-label="Search">
+                        <button class="btn btn-success bi bi-search" type="submit"></button>
+                    </form>
+                </div>
+            </div>  
+        </nav>
+        <main>
+            <div class="container">
+                <div class="squares square1">
+                    <div id="content">
+                        <i class="fa-solid fa-1"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>1° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="squares square2">
+                    <div id="content">
+                        <i class="fa-solid fa-2"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>2° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="squares square3">
+                    <div id="content">
+                        <i class="fa-solid fa-3"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>3° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div> 
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="squares square4">
+                    <div id="content">
+                        <i class="fa-solid fa-4"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>4° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="squares square5">
+                    <div id="content">
+                        <i class="fa-solid fa-5"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>5° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="squares square6">
+                    <div id="content">
+                        <i class="fa-solid fa-6"></i>
+                    </div>
+                    <div id="background">
+                        <div id="text">
+                            <h2>6° año</h2>
+                            <form action="registrar.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <button type="button" class="btn" name="estudientes" id="estudientes">Lista de Estudiantes</button>
+                                        <label for="fil_fecha_desde" class="control-label col-sm-3">Materias</label>
+                                        <select name="materia" id="materia" class="form-control">
+                                            <option value="">Matematicas</option>
+                                            <option value="">Fisica</option>
+                                            <option value="">Quimica</option>
+                                            <option value="">Ingles</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script src="https://kit.fontawesome.com/5818af7131.js" crossorigin="anonymous"></script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        <script src="https://kit.fontawesome.com/5818af7131.js" crossorigin="anonymous"></script>
+        <script>    
+            $('#materia').select2();
+        </script>
+        <footer>
+            <a href="https://lacandelaria.com.ve/" target="_blank">
+                <h3>U.E.P A.P.E.P "La Candelaria" | 2021-2023</h3>
+            </a>
+            <h4>Made by &copy; Leonardo</h4>
+        </footer>
+        <script src="https://kit.fontawesome.com/5818af7131.js" crossorigin="anonymous"></script>
+    </body>
+</html>
